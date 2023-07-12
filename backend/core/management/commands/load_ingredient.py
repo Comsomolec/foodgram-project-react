f35d523befdbe549003ac09a3e97dd75f8c0eaca
Comsomolec <sssabirzyanov@yandex.ru>
2023-07-12 22:54:45 +0300

    change load_ingredient

--- backend/core/management/commands/load_ingredient.py
+++ backend/core/management/commands/load_ingredient.py
@@ -4,12 +4,14 @@ from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
+ROUTE = 'core/data/ingredients.csv'
+
+
 class Command(BaseCommand):
     help = 'Загрузка ингредиентов из csv файла'
 
     def handle(self, *args, **options):
-        with open(
-            'core/data/ingredients.csv', 'r', encoding='UTF-8') as csv_file:
+        with open(ROUTE, 'r', encoding='UTF-8') as csv_file:
             csvreader = csv.reader(csv_file)
             for row in csvreader:
                 Ingredient.objects.bulk_create(
