d8c366bc4b6e209b8e7a6a168396158a63fb1d89
Comsomolec <sssabirzyanov@yandex.ru>
2023-07-13 22:19:49 +0300

    edit filters

--- backend/core/filters.py
+++ backend/core/filters.py
@@ -1,6 +1,6 @@
 from django_filters.filters import (
     BooleanFilter,
-        ModelChoiceField,
+    ModelChoiceField,
     ModelMultipleChoiceFilter
 )
 from django_filters.rest_framework import FilterSet
